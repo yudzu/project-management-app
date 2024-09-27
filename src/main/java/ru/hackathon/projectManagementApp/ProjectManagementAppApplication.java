77c17d27b670b8e61c65f028948939b68bb67128
steph <van.stepa@mail.ru>
2024-09-27 19:12:19 +0300

    initial commit

---
+++ src/main/java/ru/hackathon/projectManagementApp/ProjectManagementAppApplication.java
@@ -0,0 +1,13 @@
+package ru.hackathon.projectManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hackathon/projectManagementApp/ProjectManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hackathon.projectManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
